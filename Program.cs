5350d05fbbc814378adccdd70dd400d498d041d5
Pritisha123 <adhikaripritisa926@gmail.com>
2024-11-08 13:00:32 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Programa
+{
+    public static void Main(string[] arg)
+    {
+        int Soma = 0;
+        for(int a=0; a<=50; a ++ )
+        {
+            if(a % 2 != 0)
+            {
+                Soma = Soma + a;
+                
+            }
+
+        }
+
+        Console.WriteLine("Total Numeros impares=" + Soma);
+    }
+    
+}
---
+++ Program1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+//write excersise 1
+public class programe
+{
+    public static void Main(string[] arg)
+
+    {
+        for (int a = 0; a <= 20; a++)
+        {
+            if (a % 2 == 0)
+            {
+                Console.WriteLine(a);
+
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
